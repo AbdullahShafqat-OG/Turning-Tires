6f632c8dc13d70fb88b1991508f195eeb7e2d07f
Abdullah Shafqat <abdullahshafqat27@hotmail.com>
2023-07-22 15:43:52 +0500

    added powerup duration UI + powerup blink effect

--- Assets/Scripts/Powerups/ShieldPowerup.cs
+++ Assets/Scripts/Powerups/ShieldPowerup.cs
@@ -8,12 +8,26 @@ public class ShieldPowerup : MonoBehaviour
     [SerializeField]
     private GameObject _shieldGameobject;
 
+    [SerializeField]
+    private Material _blinkMaterial;
+    [SerializeField]
+    private float _bufferTime = 1.0f;
+    [SerializeField]
+    private int _blinkFrequency = 1;
+
     private CarController _carController;
 
     private bool _contactingObstacle = false;
+    private MeshRenderer _meshRenderer;
+    private Material _defaultMaterial;
+
+    private Coroutine _deactivate = null;
 
     private void Awake()
     {
+        _meshRenderer = _shieldGameobject.GetComponent<MeshRenderer>();
+        _defaultMaterial = _meshRenderer.material;
+
         _carController = GetComponent<CarController>();
 
         _shieldGameobject.SetActive(_carController.shield);
@@ -23,6 +37,8 @@ public class ShieldPowerup : MonoBehaviour
     {
         _shieldGameobject.SetActive(true);
         _carController.shield = true;
+
+        _meshRenderer.material = _defaultMaterial;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,10 +51,46 @@ public class ShieldPowerup : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (_contactingObstacle)
+        if (_contactingObstacle && _deactivate == null)
         {
-            _shieldGameobject.SetActive(false);
-            _carController.shield = false;
+            _deactivate = StartCoroutine(DetonateCoroutine());
+        }
+    }
+
+    private void Deactivate()
+    {
+        _shieldGameobject.SetActive(false);
+        _carController.shield = false;
+
+        _deactivate = null;
+    }
+
+    private IEnumerator DetonateCoroutine()
+    {
+        float elapsedTime = 0;
+        float frameCount = 0;
+
+        while (elapsedTime <= _bufferTime)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            frameCount++;
+
+            var blink = Mathf.PingPong(frameCount * Time.timeScale, 10 / _blinkFrequency);
+
+            if (blink == 10 / _blinkFrequency)
+                ToggleMaterial();
+
+            if (elapsedTime >= _bufferTime)
+                Deactivate();
+        }
+    }
+
+    private void ToggleMaterial()
+    {
+        if (_blinkMaterial != null)
+        {
+            _meshRenderer.material = _meshRenderer.material == _defaultMaterial ? _blinkMaterial : _defaultMaterial;
         }
     }
 }
